f7a9cd882cbee2487a6c71a862045d1cb02c519d
DanteTerry <sinasa88@gmail.com>
2024-06-03 23:12:02 +0200

    footer refactored

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import { FaCode } from "react-icons/fa6";
+import { FaCode } from "react-icons/fa";
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer className="mx-auto mt-3  w-max">
+    <footer className="mx-auto mt-3 w-max">
       <p className="flex items-center gap-2 text-center text-lg">
         <FaCode className="text-border" /> Designed and Coded by Arpit
       </p>
     </footer>
   );
-}
+};
 
 export default Footer;
